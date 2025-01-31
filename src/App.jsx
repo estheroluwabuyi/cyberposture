d787c788e0d9098bbfde8808ff8a195fbe8baff3
Esther Okiki <estherokiki15@gmail.com>
2025-01-31 20:50:31 +0100

    Refactored App Component to Handle Routing and Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Service from "./pages/Service";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Schedule from "./pages/Schedule";
-import NoPage from "./pages/NoPage";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppContent from "./components/AppContent";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Header>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="home" element={<Home />} />
-            <Route path="service" element={<Service />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
-            {/* <Route path="schedule" element={<Schedule />} /> */}
-            <Route path="*" element={<NoPage />} />
-          </Routes>
-        </Header>
-      </div>
+      <AppContent />
     </Router>
   );
 }
