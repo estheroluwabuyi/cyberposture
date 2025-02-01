fce18378c55c66b6d9e9b9562933245c3ac8a5d9
Esther Okiki <estherokiki15@gmail.com>
2025-02-01 03:18:10 +0100

    Fix brand image display issue

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -2,32 +2,32 @@ import SectionHeading from "./SectionHeading";
 
 const brands = [
   {
-    src: "public/brands/Cyberation-Logo.png",
+    src: "public/Cyberation-Logo.png",
     alt: "cyberation logo",
     class: "img-1",
   },
   {
-    src: "public/brands/Esusu-Logo.png",
+    src: "public/Esusu-Logo.png",
     alt: "esusu logo",
     class: "img-2",
   },
   {
-    src: "public/brands/Cellulant-Logo.png",
+    src: "public/Cellulant-Logo.png",
     alt: "cellulant logo",
     class: "img-3",
   },
   {
-    src: "public/brands/Digiss-Logo.png",
+    src: "public/Digiss-Logo.png",
     alt: "Digiss logo",
     class: "img-4",
   },
   {
-    src: "public/brands/Cyberation-Logo.png",
+    src: "public/Cyberation-Logo.png",
     alt: "cyberation logo",
     class: "img-1",
   },
   {
-    src: "public/brands/Cyberation-Logo.png",
+    src: "public/Cyberation-Logo.png",
     alt: "cyberation logo",
     class: "img-1",
   },
@@ -39,7 +39,7 @@ function Brands() {
       <SectionHeading>Organizations that trust us</SectionHeading>
       <div className="brands-container">
         {brands.map((brand, index) => (
-          <div key={index} className={brand.class}>
+          <div key={index} className={brand.c}>
             <img src={brand.src} alt={brand.alt} width="100" height="100" />
           </div>
         ))}
