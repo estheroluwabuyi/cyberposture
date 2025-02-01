07ef80eb179a4442ce7f736fed7792ad4d6a72a7
Esther Okiki <estherokiki15@gmail.com>
2025-02-01 03:12:38 +0100

    Create brands table

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,13 +1,48 @@
 import SectionHeading from "./SectionHeading";
 
+const brands = [
+  {
+    src: "public/brands/Cyberation-Logo.png",
+    alt: "cyberation logo",
+    class: "img-1",
+  },
+  {
+    src: "public/brands/Esusu-Logo.png",
+    alt: "esusu logo",
+    class: "img-2",
+  },
+  {
+    src: "public/brands/Cellulant-Logo.png",
+    alt: "cellulant logo",
+    class: "img-3",
+  },
+  {
+    src: "public/brands/Digiss-Logo.png",
+    alt: "Digiss logo",
+    class: "img-4",
+  },
+  {
+    src: "public/brands/Cyberation-Logo.png",
+    alt: "cyberation logo",
+    class: "img-1",
+  },
+  {
+    src: "public/brands/Cyberation-Logo.png",
+    alt: "cyberation logo",
+    class: "img-1",
+  },
+];
+
 function Brands() {
   return (
     <section className="brands">
       <SectionHeading>Organizations that trust us</SectionHeading>
-
       <div className="brands-container">
-        <img src="" alt="cyberation logo" width="100" height="100" />
-        <img src="" alt="cyberation logo" width="100" height="100" />
+        {brands.map((brand, index) => (
+          <div key={index} className={brand.class}>
+            <img src={brand.src} alt={brand.alt} width="100" height="100" />
+          </div>
+        ))}
       </div>
     </section>
   );
